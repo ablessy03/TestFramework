ef302cc66b251475c1dd390394d4e15bcdf3a936
ablessy03 <84451727+ablessy03@users.noreply.github.com>
2021-06-30 15:46:10 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function toggle(source) {
-  checkboxes = document.getElementsByName('foo');
+  checkboxes = document.getElementsByName('users-permission');
   for(var i=0, n=checkboxes.length;i<n;i++) {
     checkboxes[i].checked = source.checked;
   }
