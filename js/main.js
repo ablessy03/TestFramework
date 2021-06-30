f2164a2c1ae80059e8ceb04921899074bb75b17c
ablessy03 <84451727+ablessy03@users.noreply.github.com>
2021-06-30 16:24:44 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ function toggle(source) {
   }
 }
 
-function toggle(source) {
+function toggle(environment) {
   checkboxes = document.getElementsByName('environment-permission');
   for(var i=0, n=checkboxes.length;i<n;i++) {
-    checkboxes[i].checked = source.checked;
+    checkboxes[i].checked = environment.checked;
   }
 }
