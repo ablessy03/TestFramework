3f536e6595d8340a7a3cbd391759535dcfd40613
ablessy03 <84451727+ablessy03@users.noreply.github.com>
2021-07-02 19:26:43 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,13 @@ function toggle(source) {
   }
 }
 
+// Connection File Type
+$(document).on('change', '.div-toggle', function() {
+  var target = $(this).data('target');
+  var show = $("option:selected", this).data('show');
+  $(target).children().addClass('hide');
+  $(show).removeClass('hide');
+});
+$(document).ready(function(){
+	$('.div-toggle').trigger('change');
+});
