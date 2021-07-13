9106fbdc1e1a2808ae5b6d7a3cdafe4af5227659
ablessy03 <84451727+ablessy03@users.noreply.github.com>
2021-07-13 17:27:32 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -17,17 +17,17 @@ $(document).ready(function(){
 });
 
 // Change Profile
-$("#profileImage").click(function(e) {
-    $("#imageUpload").click();
-});
-
-function fasterPreview( uploader ) {
-    if ( uploader.files && uploader.files[0] ){
-          $('#profileImage').attr('src', 
-             window.URL.createObjectURL(uploader.files[0]) );
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            $('#imagePreview').css('background-image', 'url('+e.target.result +')');
+            $('#imagePreview').hide();
+            $('#imagePreview').fadeIn(650);
+        }
+        reader.readAsDataURL(input.files[0]);
     }
 }
-
-$("#imageUpload").change(function(){
-    fasterPreview( this );
+$("#imageUpload").change(function() {
+    readURL(this);
 });
