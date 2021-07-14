2eb6c079bc6aa07631a616ab0c9b7074071f193a
ablessy03 <84451727+ablessy03@users.noreply.github.com>
2021-07-14 18:28:24 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -17,48 +17,23 @@ $(document).ready(function(){
 });
 
 // Change Profile
-var triggerUpload = document.getElementById('triggerUpload'),
-    upInput = document.getElementById('filePicker'),
-    preview = document.querySelector('.preview');
+$(document).ready(function() {
 
-//force triggering the file upload here...
-triggerUpload.onclick = function() {
-  upInput.click();
-};
+    
+    var readURL = function(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
 
+            reader.onload = function (e) {
+                $('.avatar').attr('src', e.target.result);
+            }
+    
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    
 
-upInput.onchange = function(e) {
-
-  var uploaded = this.value,
-      ext = uploaded.substring(uploaded.lastIndexOf('.') + 1),
-      ext = ext.toLowerCase(),
-      fileName = uploaded.substring(uploaded.lastIndexOf("\\") + 1),
-      accepted = ["jpg", "png", "gif", "jpeg"];
-  
-  /*
-    ::Add in blank img tag and spinner
-    ::Use FileReader to read the img data
-    ::Set the image source to the FileReader data
-  */
-  function showPreview() {
-      preview.innerHTML = "<div class='loadingLogo'></div>";
-	    preview.innerHTML += '<img id="img-preview" />';
-	    var reader = new FileReader();
-	    reader.onload = function () {
-	        var img = document.getElementById('img-preview');
-	        img.src = reader.result;
-	    };
-	    reader.readAsDataURL(e.target.files[0]);
-      preview.removeChild(document.querySelector('.loadingLogo'));
-      document.querySelector('.fileName').innerHTML = fileName + "<b> Uploaded!</b>";
-  };
-  
-  //only do if supported image file
-  if (new RegExp(accepted.join("|")).test(ext)) {
-    showPreview();
-  } else {
-    preview.innerHTML = "";
-    document.querySelector('.fileName').innerHTML = "Hey! Upload an image file, not a <b>." + ext + "</b> file!";
-  }
-  
-}
+    $(".file-upload").on('change', function(){
+        readURL(this);
+    });
+});
