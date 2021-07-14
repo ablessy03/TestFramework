d703cc2561abc5138d7c6b033a6aefd7b6e319e3
ablessy03 <84451727+ablessy03@users.noreply.github.com>
2021-07-14 18:05:27 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -17,20 +17,48 @@ $(document).ready(function(){
 });
 
 // Change Profile
-$(document).ready(function(){
+var triggerUpload = document.getElementById('triggerUpload'),
+    upInput = document.getElementById('filePicker'),
+    preview = document.querySelector('.preview');
 
-  $("#imageUpload").change(function(data){
+//force triggering the file upload here...
+triggerUpload.onclick = function() {
+  upInput.click();
+};
 
-    var imageFile = data.target.files[0];
-    var reader = new FileReader();
-    reader.readAsDataURL(imageFile);
 
-    reader.onload = function(evt){
-      $('#imagePreview').attr('src', evt.target.result);
-      $('#imagePreview').hide();
-      $('#imagePreview').fadeIn(650);
-    }
-    
-  });
-	
-});
+upInput.onchange = function(e) {
+
+  var uploaded = this.value,
+      ext = uploaded.substring(uploaded.lastIndexOf('.') + 1),
+      ext = ext.toLowerCase(),
+      fileName = uploaded.substring(uploaded.lastIndexOf("\\") + 1),
+      accepted = ["jpg", "png", "gif", "jpeg"];
+  
+  /*
+    ::Add in blank img tag and spinner
+    ::Use FileReader to read the img data
+    ::Set the image source to the FileReader data
+  */
+  function showPreview() {
+      preview.innerHTML = "<div class='loadingLogo'></div>";
+	    preview.innerHTML += '<img id="img-preview" />';
+	    var reader = new FileReader();
+	    reader.onload = function () {
+	        var img = document.getElementById('img-preview');
+	        img.src = reader.result;
+	    };
+	    reader.readAsDataURL(e.target.files[0]);
+      preview.removeChild(document.querySelector('.loadingLogo'));
+      document.querySelector('.fileName').innerHTML = fileName + "<b> Uploaded!</b>";
+  };
+  
+  //only do if supported image file
+  if (new RegExp(accepted.join("|")).test(ext)) {
+    showPreview();
+  } else {
+    preview.innerHTML = "";
+    document.querySelector('.fileName').innerHTML = "Hey! Upload an image file, not a <b>." + ext + "</b> file!";
+  }
+  
+}
