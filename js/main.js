3fcc00de694f3b88792a520a23c75f9dda6361c8
ablessy03 <84451727+ablessy03@users.noreply.github.com>
2021-07-14 19:42:45 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-function toggle(source) {
-  checkboxes = document.getElementsByName('users-permission');
-  for(var i=0, n=checkboxes.length;i<n;i++) {
-    checkboxes[i].checked = source.checked;
-  }
-}
-
-// Connection File Type
-$(document).on('change', '.div-toggle', function() {
-  var target = $(this).data('target');
-  var show = $("option:selected", this).data('show');
-  $(target).children().addClass('hide');
-  $(show).removeClass('hide');
-});
-$(document).ready(function(){
-	$('.div-toggle').trigger('change');
-});
-
 // Change Profile
 $(document).ready(function() {
 	
@@ -39,3 +21,23 @@ $(document).ready(function() {
        $(".file-upload").click();
     });
 });
+
+function toggle(source) {
+  checkboxes = document.getElementsByName('users-permission');
+  for(var i=0, n=checkboxes.length;i<n;i++) {
+    checkboxes[i].checked = source.checked;
+  }
+}
+
+// Connection File Type
+$(document).on('change', '.div-toggle', function() {
+  var target = $(this).data('target');
+  var show = $("option:selected", this).data('show');
+  $(target).children().addClass('hide');
+  $(show).removeClass('hide');
+});
+$(document).ready(function(){
+	$('.div-toggle').trigger('change');
+});
+
+
