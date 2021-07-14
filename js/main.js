d542bb6c1689206cefdf17fb355ac545e16f07e7
ablessy03 <84451727+ablessy03@users.noreply.github.com>
2021-07-14 20:01:12 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
-// Change Profile
-$(document).ready(function() {
-	
-    var readURL = function(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
+$(document).ready(function () {
 
-            reader.onload = function (e) {
-                $('.profile-pic').attr('src', e.target.result);
-            }
-    
-            reader.readAsDataURL(input.files[0]);
-        }
+  var readURL = function (input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        $('.profile-pic').attr('src', e.target.result);
+      };
+
+      reader.readAsDataURL(input.files[0]);
     }
-   
-    $(".file-upload").on('change', function(){
-        readURL(this);
-    });
-    
-    $(".upload-button").on('click', function() {
-       $(".file-upload").click();
-    });
+  };
+
+  $(".file-upload").on('change', function () {
+    readURL(this);
+  });
+
+  $(".upload-button").on('click', function () {
+    $(".file-upload").click();
+  });
 });
+
 
 function toggle(source) {
   checkboxes = document.getElementsByName('users-permission');
